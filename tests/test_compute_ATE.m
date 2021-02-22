2cd88540f3a37fcc23d339696c32e8bf62bd9490
Yulun Tian <yulun@mit.edu>
2021-02-22 14:10:41 -0500

    test compute_ATE

---
+++ tests/test_compute_ATE.m
@@ -0,0 +1,16 @@
+clear; clc; close all;
+d = 3;
+n = 1000;
+t_src = 100 * rand(d,n);
+
+transform = struct;
+transform.R = randrot(d);
+transform.t = rand(d,1);
+
+t_dst = transform_translations(t_src, transform);
+
+ATE = compute_ATE(t_src, t_dst);
+assert(ATE < 1e-8);
+
+t_dst = t_dst + 0.01 * randn(d,n);
+ATE = compute_ATE(t_src, t_dst);
