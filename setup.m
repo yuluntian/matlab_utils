439ff046cbe88ae67caf79094f321a0fc1851d4f
Yulun Tian <yulun@mit.edu>
2022-08-03 13:14:58 -0400

    make manopt import optional

--- setup.m
+++ setup.m
@@ -1,7 +1,11 @@
 %% Dependency
 manopt_dir = '/home/yulun/git/Manopt_7.0/manopt';
 manopt_import_file = fullfile(manopt_dir, 'importmanopt.m');
-run(manopt_import_file);
+if isfile(manopt_import_file)
+    run(manopt_import_file);
+else
+    fprintf('Manopt not found. Some functions might not work. \n');
+end
 
 %% Add this directory
 addpath(genpath(pwd));
