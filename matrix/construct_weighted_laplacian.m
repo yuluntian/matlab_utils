bcc11ce99be96afc3417f5e2e876f9b02ce2605f
Yulun Tian <yulun@mit.edu>
2022-08-29 22:21:04 -0400

    improve efficiency of laplacian impl

--- matrix/construct_weighted_laplacian.m
+++ matrix/construct_weighted_laplacian.m
@@ -6,12 +6,14 @@
 function L = construct_weighted_laplacian(nodes, edges, weights)
 
 n = length(nodes);
-r = [];
-c = [];
-v = [];
+m = size(edges, 1);
+r = zeros(1, 4*m);  % Each edge adds 4 values to the Laplacian matrix
+c = zeros(1, 4*m);
+v = zeros(1, 4*m);
 assert(length(weights) == size(edges, 1));
 
 for k = 1:size(edges,1)
+    offset = (k-1) * 4;
     v1 = edges(k,1);
     v2 = edges(k,2);
     w = weights(k);
@@ -20,21 +22,21 @@ for k = 1:size(edges,1)
     assert(length(i1) == 1)
     assert(length(i2) == 1)
     
-    r(end+1) = i1;
-    c(end+1) = i1;
-    v(end+1) = w;
+    r(offset+1) = i1;
+    c(offset+1) = i1;
+    v(offset+1) = w;
     
-    r(end+1) = i1;
-    c(end+1) = i2;
-    v(end+1) = -w;
+    r(offset+2) = i1;
+    c(offset+2) = i2;
+    v(offset+2) = -w;
 
-    r(end+1) = i2;
-    c(end+1) = i1;
-    v(end+1) = -w;
+    r(offset+3) = i2;
+    c(offset+3) = i1;
+    v(offset+3) = -w;
 
-    r(end+1) = i2;
-    c(end+1) = i2;
-    v(end+1) = w;
+    r(offset+4) = i2;
+    c(offset+4) = i2;
+    v(offset+4) = w;
 
 end
 
