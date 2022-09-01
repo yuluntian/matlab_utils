81c15991edcc6a0afcc55bec776569feb443a9f6
Yulun Tian <yulun@mit.edu>
2022-09-01 14:46:52 -0400

    ...

--- tests/test_matrix/test_extract_blkdiag.m
+++ tests/test_matrix/test_extract_blkdiag.m
@@ -1,7 +1,14 @@
 clear; clc; close all;
 
+% Test same block sizes
 p = 2;
 num_blocks = 4;
 n = p * num_blocks;
-A = randn(n,n);
-D = extract_blkdiag_matrix(A, p);
+A1 = randn(n,n);
+D1 = extract_blkdiag_matrix(A1, p);
+
+% Test different block sizes
+bdims = [1 5 3];
+n = sum(bdims);
+A2 = randn(n,n);
+D2 = extract_blkdiag_matrix(A2, bdims);
