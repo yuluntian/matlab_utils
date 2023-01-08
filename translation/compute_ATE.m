b6affa98a3c9d29fcf962cc4af7b5097e84c764f
Yulun Tian <yulun@mit.edu>
2023-01-08 10:55:37 -0500

    update ATE script to optionally align inputs

--- translation/compute_ATE.m
+++ translation/compute_ATE.m
@@ -1,16 +1,24 @@
-% function ATE = compute_ATE(translations1, translations2)
-% Given two sets of positions, align the positions and then compute the
-% RMSE absolute trajectory error.
+% function ATE = compute_ATE(translations1, translations2, options)
+% Given two sets of positions, compute the RMSE absolute trajectory error.
 %
 % Each set of translation is a d-by-n matrix, where n is the number of
 % points.
+%
+% options.align: if set to true, will first align the input positions in
+% the global frame before computing ATE.
 %
 % Definition of ATE:
 % Sturm et al. "A Benchmark for the Evaluation of RGB-D SLAM Systems"
 % 
 % Yulun Tian 
 
-function ATE = compute_ATE(translations1, translations2)
+function ATE = compute_ATE(translations1, translations2, options)
+if nargin < 3
+    options = struct;
+end
+if ~isfield(options, 'align')
+    options.align = true;
+end
 
 d = size(translations1, 1);
 n = size(translations1, 2);
@@ -18,7 +26,9 @@ assert(size(translations2, 1) == d);
 assert(size(translations2, 2) == n);
 
 % Align translations
-[~, translations1] = align_translations(translations1, translations2);
+if options.align
+    [~, translations1] = align_translations(translations1, translations2);
+end
 
 % Compute the 2-norm for each column
 distances = vecnorm(translations1 - translations2, 2, 1);
